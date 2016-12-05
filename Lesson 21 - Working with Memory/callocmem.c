01c2be504cd4efe968e951361bbcb1aae1a4f2d8
Paul McQuade <paulmcquad@gmail.com>
2016-12-05 20:52:41 +0000

    Finished Lesson 21
    Working with Memory
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ Lesson 21 - Working with Memory/callocmem.c
@@ -0,0 +1,22 @@
+/* Demonstrates calloc(). */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+    unsigned long num;
+    int *ptr;
+
+    printf("Enter the number of type int to allocate: ");
+    scanf("%ld", &num);
+
+    ptr = (int*)calloc(num, sizeof(long long));
+
+    if (ptr != NULL)
+        puts("Memory allocation was successful.");
+    else
+        puts("Memory allocation failed.");
+    return 0;
+}
+
---
+++ Lesson 21 - Working with Memory/casting.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	
+  int i1 = 100, i2 = 40;
+  float f1;
+
+  f1 = i1/i2;
+  //  f1 = (float)i1/i2;
+  printf("%f\n", f1);
+  return 0;
+}
